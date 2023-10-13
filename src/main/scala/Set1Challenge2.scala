6e8e02ba229b44aead8debae9b597c44ae2bb48f
Daniel Le <greenrecyclebin@gmail.com>
2023-10-13 18:53:17 +0800

    Set 1 - Challenge 2

---
+++ src/main/scala/Set1Challenge2.scala
@@ -0,0 +1,13 @@
+package io.github.greenrecyclebin.cryptopals
+
+object Set1Challenge2:
+  def main(args: Array[String]): Unit =
+    Integer.parseInt("c", 16)
+    val hex1 = "1c0111001f010100061a024b53535009181c"
+    val hex2 = "686974207468652062756c6c277320657965"
+    val xor = Hex.xor(hex1, hex2)
+
+    println(hex1)
+    println(hex2)
+    println(xor)
+    println(xor == "746865206b696420646f6e277420706c6179")
