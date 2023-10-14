86fbfa2e5c742f6931177d56afbd1ccd6e8996e8
Daniel Le <greenrecyclebin@gmail.com>
2023-10-14 14:47:20 +0800

    Set 1 - Challenge 3

---
+++ src/main/scala/Set1Challenge3.scala
@@ -0,0 +1,9 @@
+package io.github.greenrecyclebin.cryptopals
+
+object Set1Challenge3:
+  def main(args: Array[String]): Unit =
+    val ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    val plaintexts = SingleByteXorCipher.decryptHex(ciphertext).sortBy(_.score).toList
+
+    println(ciphertext)
+    println(plaintexts.mkString("\n"))
---
+++ src/main/scala/SingleByteXorCipher.scala
@@ -0,0 +1,13 @@
+package io.github.greenrecyclebin.cryptopals
+
+import scala.collection.{IndexedSeqView, SeqView}
+
+object SingleByteXorCipher:
+  def decryptHex(ciphertext: String): SeqView[Plaintext] =
+    (0 to Byte.MaxValue).view.map(_.toByte).map(b => {
+      val plaintext = Hex.xorToChars(ciphertext, b)
+
+      Plaintext(b, plaintext, TextScorer.score(plaintext))
+    })
+
+  case class Plaintext(key: Byte, plaintext: String, score: Int)
