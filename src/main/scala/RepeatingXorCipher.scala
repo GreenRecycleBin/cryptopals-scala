73b3aabee5760356d9a6ab25afad9e5feded1a6e
Daniel Le <greenrecyclebin@gmail.com>
2023-10-19 09:01:53 +0800

    Set 1 - Challenge 6

--- src/main/scala/RepeatingXorCipher.scala
+++ src/main/scala/RepeatingXorCipher.scala
@@ -18,3 +18,44 @@ object RepeatingXorCipher:
         .map(_ ^ _)
         .map(_.toByte)
     )
+
+  def decrypt(bs: Seq[Byte]): (String, String) =
+    val keySizes = (2 to 40)
+      .map(keySize =>
+        (keySize, HammingDistance.averageHammingDistance(bs, keySize))
+      )
+      .sortBy(_._2)
+      .map(_._1)
+
+    val keys = keySizes
+      .take(3)
+      .map(keySize =>
+        val transposedBs = bs.grouped(keySize).map(_.toArray).toArray.transpose
+
+        transposedBs.map { b =>
+          val plaintextBytes = SingleByteXorCipher
+            .decrypt(b)
+            .sortBy(_.score)
+            .collectFirst(identity)
+            .get
+
+          plaintextBytes.key
+        }
+      )
+
+    val (key, plaintext, _) = keys
+      .map(key => (key, decrypt(bs, key)))
+      .map((key, plaintext) => (key, plaintext, TextScorer.score(plaintext)))
+      .sortBy(_._3)
+      .collectFirst(identity)
+      .get
+
+    (String(key, StandardCharsets.ISO_8859_1), plaintext)
+
+  private def decrypt(bs: Seq[Byte], key: Seq[Byte]): String =
+    val repeatingKeys = cycle(key)
+
+    String(
+      bs.zip(repeatingKeys).map(_ ^ _).map(_.toByte).toArray,
+      StandardCharsets.ISO_8859_1
+    )
---
+++ src/main/scala/Set1Challenge6.scala
@@ -0,0 +1,26 @@
+package io.github.greenrecyclebin.cryptopals
+
+import RepeatingXorCipher.decrypt
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
+object Set1Challenge6:
+  def main(args: Array[String]): Unit =
+    val s1 = "this is a test"
+    val s2 = "wokka wokka!!!"
+    val distance = HammingDistance(s1, s2)
+
+    println(s"HammingDistance($s1, $s2) = $distance")
+    println(distance == 37)
+
+    val s = Files
+      .readString(
+        Paths.get(
+          "/Users/greenrecyclebin/Programming/Scala/Cryptopals/src/main/scala/6.txt"
+        ),
+        StandardCharsets.ISO_8859_1
+      )
+      .replace("\n", "")
+
+    println(decrypt(java.util.Base64.getDecoder.decode(s)))
